6eb836fd252541705256aa544966a5bbd145d5ff
Vadim Filipovsky <v.filipovsky@dyninno.lv>
2020-01-13 11:51:33 +0200

    chat_id and increase MAX_LINKS_PER_CHAT to 7

--- storage.py
+++ storage.py
@@ -1,37 +1,37 @@
 from collections import defaultdict
 
 
-MAX_LINKS_PER_USER = 3
+MAX_LINKS_PER_CHAT = 7
 SETTINGS = defaultdict(dict)
 
 
-def update_last_ad_id(user_id, link_name, last_ad_id):
-    SETTINGS[user_id][link_name]['last_ad_id'] = last_ad_id
+def update_last_ad_id(chat_id, link_name, last_ad_id):
+    SETTINGS[chat_id][link_name]['last_ad_id'] = last_ad_id
 
 
-def get_links(user_id):
-    if user_id not in SETTINGS or len(SETTINGS[user_id]) == 0:
+def get_links(chat_id):
+    if chat_id not in SETTINGS or len(SETTINGS[chat_id]) == 0:
         raise Exception('You need to add valid link first')
 
-    return SETTINGS[user_id]
+    return SETTINGS[chat_id]
 
 
-def save_link(user_id, link, link_name, last_ad_id):
-    if user_id in SETTINGS:
-        if link_name in SETTINGS[user_id]:
+def save_link(chat_id, link, link_name, last_ad_id):
+    if chat_id in SETTINGS:
+        if link_name in SETTINGS[chat_id]:
             raise Exception('This name is already in use')
 
-        if len(SETTINGS[user_id]) >= MAX_LINKS_PER_USER:
-            raise Exception('You have maximum available links saved')
+        if len(SETTINGS[chat_id]) >= MAX_LINKS_PER_CHAT:
+            raise Exception('You have maximum available links saved per chat')
 
-    SETTINGS[user_id][link_name] = {'link': link, 'last_ad_id': last_ad_id}
+    SETTINGS[chat_id][link_name] = {'link': link, 'last_ad_id': last_ad_id}
 
 
-def del_link(user_id, link_name):
-    if user_id not in SETTINGS:
+def del_link(chat_id, link_name):
+    if chat_id not in SETTINGS:
         raise Exception('You dont have any saved links')
 
-    if link_name not in SETTINGS[user_id]:
+    if link_name not in SETTINGS[chat_id]:
         raise Exception(f'{link_name} not exists')
 
-    del SETTINGS[user_id][link_name]
+    del SETTINGS[chat_id][link_name]
