1547f7b11eece65d0f168290a18e7d34076c1e3f
Vadim Filipovsky <vadim.filipovsky@gmail.com>
2020-03-17 18:04:02 +0200

    cs

--- notifier_bot.py
+++ notifier_bot.py
@@ -5,9 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-updater = Updater(token=os.getenv('TELEGRAM_TOKEN'), use_context=True, request_kwargs={
-    'read_timeout': 30, 'connect_timeout': 30
-})
+updater = Updater(token=os.getenv('TELEGRAM_TOKEN'),
+                  use_context=True,
+                  request_kwargs={
+                        'read_timeout': 30, 'connect_timeout': 30})
 
 dispatcher = updater.dispatcher
 job_queue = updater.job_queue
@@ -17,7 +18,9 @@ dispatcher.add_handler(CommandHandler('add', command.add))
 dispatcher.add_handler(CommandHandler('del', command.delete))
 dispatcher.add_handler(CommandHandler('links', command.get_user_links))
 
-job_queue.run_repeating(command.notify_job, interval=int(os.getenv('INTERVAL')), first=3)
+job_queue.run_repeating(command.notify_job,
+                        interval=int(os.getenv('INTERVAL')),
+                        first=3)
 
 
 if __name__ == '__main__':
